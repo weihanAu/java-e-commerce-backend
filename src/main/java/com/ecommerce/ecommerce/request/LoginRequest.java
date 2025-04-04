9554fd276ec709781d0cedfaced67cbdca88aa54
Hudson <hudson.sydney.software@gmail.com>
2025-04-04 16:43:53 +1100

    final

---
+++ src/main/java/com/ecommerce/ecommerce/request/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.ecommerce.ecommerce.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+  @NotBlank
+  private String email;
+  @NotBlank
+  private String password;
+}
